76fe3264bdfa91af179303ee1e5d725631e44798
alizhabr <105541564+alizhabr@users.noreply.github.com>
2022-05-14 12:59:46 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
 current_state = [
     ["X", " ", "O"],
-    ["O", "X", "X"],
-    ["X", " ", "O"]
+    ["O", "X", "O"],
+    ["X", " ", "X"]
 ]
-
-
 
 
 def green(s):
@@ -17,6 +15,31 @@ def print_filed(field):
         print(green(item))
 
 
+def check_possible_move(x, y, letter):
+#     if field is empty -> return True
+#     else False
+#     also check letter TODO
+    if current_state[x][y] == " ":
+        return True
+    else:
+        return False
+
+
+def move(x, y, letter):
+    current_state[x][y]=letter
+
+
 if __name__ == '__main__':
     print_filed(current_state)
+    print()
+    move(0, 1, "O")
+    print_filed(current_state)
+    print()
+    x, y, letter = 0, 0, "X"
+
+    # обурнуть в ф-ю TODO
+    if check_possible_move(x, y, letter):
+        move(x, y, letter)
+    else:
+        print("Forbidden action {} {} {}".format(x, y, letter))
 
